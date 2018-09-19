2f5f325d3158cd0c699cbbc27ac7d832e30356da
Mary Han <maryzhenghan@gmail.com>
2018-09-19 10:31:50 -0700

    Add travis integration

--- src/components/card.test.js
+++ src/components/card.test.js
@@ -5,14 +5,12 @@ import Card from './card';
 
 describe('<Card />', () => {
     it('Renders without crashing', () => {
-        shallow(<Card text="Foo" />);
+      shallow(<Card text="Foo" />);
     });
 
     it('Renders the text', () => {
-        const text = "Foo";
-        const wrapper = shallow(<Card text={text} />);
-        expect(wrapper.text()).toEqual(text);
+      const text = "Foo";
+      const wrapper = shallow(<Card text={text} />);
+      expect(wrapper.text()).toEqual(text);
     });
 });
-
-
